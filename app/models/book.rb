8b63a2ff6d52352768527f8d13b0f29deb3d3eab
hassy1220 <ec2-user@ip-172-31-41-57.ec2.internal>
2022-03-07 11:59:15 +0000

    完成

---
+++ app/models/book.rb
@@ -0,0 +1,10 @@
+class Book < ApplicationRecord
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+
+  belongs_to:user
+  # def get_profile_image
+  #   (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  # end
+
+end
