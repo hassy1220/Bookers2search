da967f4513487d37fedaa33bf8ef2a77d6fce3d1
hassy1220 <ec2-user@ip-172-31-41-57.ec2.internal>
2022-03-12 08:06:58 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
   has_many:books,dependent: :destroy
   has_many:favorites,dependent: :destroy
   has_many:book_comments,dependent: :destroy
+
+  has_many:follower,class_name: "Relationship" , foreign_key: "follower_id",dependent: :destroy
+  has_many:followed,class_name: "Relationship" , foreign_key: "followed_id",dependent: :destroy
+
+  has_many:followers,through: :follower,source: :followed
+  has_many:followeds,through: :followed,source: :follower
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
     resource:favorites,only:[:create, :destroy]
     resources:book_comments,only:[:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource:relationship,only:[:create, :destroy]
+    get "relationships/follow_list"
+    get "relationships/follower_list"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
