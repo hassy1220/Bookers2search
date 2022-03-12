303a34b87d18de0ca3c873b0c6ec2118a3e95601
hassy1220 <ec2-user@ip-172-31-41-57.ec2.internal>
2022-03-12 15:17:01 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
+  get '/search', to: 'searches#searche'
   get "home/about"=>"homes#about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
@@ -13,6 +14,8 @@ Rails.application.routes.draw do
     get "relationships/follow_list"
     get "relationships/follower_list"
   end
+  # get '/search'=>'searches#search',as:"search"
+  # get '/search', to: 'searchs#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -87,3 +87,8 @@ ActiveRecord::Schema.define(version: 2022_03_08_082706) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
+
+
+
+
+
